f946f11f083731158ed30aa0d3cf37f6c15dbfd8
Santiago <juniorsantiagoalvarez03@gmail.com>
2024-08-22 09:15:18 -0600

    Actualizando Ruta de Inicio

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import EventList from "./components/EventList/EventList";
+// import EventList from "./components/EventList/EventList";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <EventList />
+    <Header />
+    <HomePage />
+    <Footer />
     <ToastContainer />
   </React.StrictMode>
 );
