658485d5d83ba0744d55fbcbc5649f7bab0be704
Santiago <juniorsantiagoalvarez03@gmail.com>
2024-08-22 11:47:13 -0600

    Actualizando el enrutamiento de la página

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import EventList from "./components/EventList/EventList";
+import EventList from "./components/EventList/EventList";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <HomePage />
-    <Footer />
-    <ToastContainer />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events" element={<EventList />} />
+      </Routes>
+      <Footer />
+      <ToastContainer />
+    </Router>
   </React.StrictMode>
 );
