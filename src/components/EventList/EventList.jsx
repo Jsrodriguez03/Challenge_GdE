1ea5f049773b1cc928bb8b761dd99b690160b2b3
Santiago <juniorsantiagoalvarez03@gmail.com>
2024-08-21 19:41:54 -0600

    Actualizando Diseño de la Vista Eventos

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,32 +1,14 @@
+import { useState, useEffect } from "react";
 import "./EventList.css";
+import eventsData from "../../data/events.json"; // Importa directamente el JSON
 
 export default function EventList() {
-  const events = [
-    {
-      title: "Exposición Rodolfo Nieto",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sit deserunt laborum, incidunt delectus ea autem dignissimos repellendus qui! Quo reiciendis harum aliquid. Excepturi repellendus corporis ut, iure eaque ratione.",
-      date: "Lunes, 19 de Agosto",
-      time: "10:00 AM",
-      location: "Plaza Nativa",
-    },
-    {
-      title: "Fería Lanzate",
-      description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic officiis autem porro architecto totam. Expedita cum odio dolores mollitia velit eaque obcaecati placeat, eius, ratione nobis est sed voluptatibus. Ipsum!",
-      date: "Martes, 20 de Agosto",
-      time: "10:00 AM",
-      location: "Plaza Nativa",
-    },
-    {
-      title: "Concierto Morat",
-      description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque fugit quas consectetur ad deleniti obcaecati voluptates. Officiis ipsa laudantium nesciunt impedit laboriosam, vel reiciendis, odit possimus, cum maiores est nam.",
-      date: "Miércoles, 22 de Agosto",
-      time: "03:00 PM",
-      location: "Plaza Nativa",
-    },
-  ];
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    // Establecer los eventos desde el JSON importado
+    setEvents(eventsData);
+  }, []);
 
   return (
     <div className="container-event-list">
@@ -39,12 +21,12 @@ export default function EventList() {
             <th scope="col">Fecha</th>
             <th scope="col">Hora</th>
             <th scope="col">Lugar</th>
-            <th scope="col">Acción</th>
+            <th scope="col">Inscribirse</th>
           </tr>
         </thead>
         <tbody>
-          {events.map((event, index) => (
-            <tr key={index}>
+          {events.map((event) => (
+            <tr key={event.id}>
               <td>{event.title}</td>
               <td>{event.description}</td>
               <td>{event.date}</td>
